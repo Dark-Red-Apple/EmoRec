302c916ad227a7bedf6982470a01d85b86bbe73c
Dark_Red_Apple <alma6828@gmail.com>
2022-06-28 21:38:53 -0400

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import string
 nltk.download("stopwords")
 # # import matplotlib.pyplot as plt
 
-model = TFDistilBertForSequenceClassification.from_pretrained("/", local_files_only=True)
+model = TFDistilBertForSequenceClassification.from_pretrained("./saved_model", local_files_only=True)
 # tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
 # try:    
 
